7df074a885e81a3aa35e76eaa7e261dd5fa4a8ab
Daniil <105662394+Daniil10295@users.noreply.github.com>
2025-04-05 23:06:31 +0300

    upd

--- config.ts
+++ config.ts
@@ -1,4 +1,4 @@
-// import token from './public.pem';
+//import token from './public.pem';
 
 export const window = {
     width: 900,
@@ -11,9 +11,9 @@ export const window = {
 };
 
 export const api = {
-    ws: 'ws://127.0.0.1:1370/ws',
-    web: 'http://127.0.0.1:1370',
-    // extraToken: token,
+    ws: 'ws://46.138.247.75:1370/ws',
+    web: 'http://46.138.247.75:1370',
+    //extraToken: token,
 };
 
 export const appPath = '.aurora-launcher';
